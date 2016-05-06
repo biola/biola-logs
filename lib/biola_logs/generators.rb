d6056735988f36f53f1a09b8a901ca9f90019572
Jeff Silzer <jeff.silzer@biola.edu>
2016-05-06 14:20:08 -0700

    added support for logging host, request_id (aka the request.uuid in Rails), and session_id

--- lib/biola_logs/generators.rb
+++ lib/biola_logs/generators.rb
@@ -1,10 +1,15 @@
 module BiolaLogs
   module Generators
     def self.all
+      # Class method to get all classes in the BiolaLogs::Generators module 
+      # returns an Array, skipping BiolaLogs::Generators::Base
+      # example: [BiolaLogs::Generators::Params, BiolaLogs::Generators::Headers] 
       self.constants.map(&self.method(:const_get)).grep(Class) - [Base]
     end
 
     def self.handler
+      # Class method that returns a hash of key/value pairs for `config.lograge.custom_options`
+      # Calls to_hash() on all generators and merges the results into a single hash
       Proc.new do |event|
         self.all.each_with_object({}) do |klass, hash|
           hash.merge! klass.new(event).to_hash
